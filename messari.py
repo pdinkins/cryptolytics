709232d017d5651f6c3d06ac603bca10f1d62ae2
pdinkins <parkerdinkins@gmail.com>
2021-02-08 15:13:54 -0500

    Update messari.py

--- messari.py
+++ messari.py
@@ -1,38 +1,18 @@
 import requests
 
 apiURL = "https://data.messari.io/api/v1/assets?fields=id,slug,symbol,metrics/market_data/price_usd"
+apiURL_BTC = "https://data.messari.io/api/v1/assets/bitcoin/metrics"
 
 response = requests.get(apiURL).json()
-#print(type(response))
-
-keys = response.keys()
-keys_list = list()
-for i in keys:
-    keys_list.append(i)
-
-data_response = response[keys_list[1]]
-#print(response[keys_list[1]])
-#print(data_response)
-#print(type(data_response))
-
-l2_keys = data_response[0].keys()
-l2_keys_list = list()
-for i in l2_keys:
-    l2_keys_list.append(i)
-
-def api_dump():
-    for i in range (0,len(data_response)):
-        for j in range(2,4):
-            print(data_response[i][l2_keys_list[j]])
 
 def bitcoin_price():
-    return data_response[0][l2_keys_list[3]]['market_data']['price_usd']
+    return response['data'][0]['metrics']['market_data']['price_usd']
 
 def ethereum_price():
-    return data_response[1][l2_keys_list[3]]['market_data']['price_usd']
+    return response['data'][1]['metrics']['market_data']['price_usd']
 
 def link_price():
-    return data_response[9][l2_keys_list[3]]['market_data']['price_usd']
+    return response['data'][9]['metrics']['market_data']['price_usd']
 
 
 def test_price_oracles():
